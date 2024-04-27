acedd36848db20e5d853635ad875966feef16cf4
s111110512 <peter930709@gmail.com>
2024-04-28 00:22:37 +0800

    re

---
+++ 0410/norace.c
@@ -0,0 +1,47 @@
+﻿#include <stdio.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+#define LOOPS 10000
+int money = 100;
+
+void *ATM1()
+{
+  for (int i=0; i<LOOPS; i++) {
+    pthread_mutex_lock( &mutex1 );
+    money = money + 1;
+    pthread_mutex_unlock( &mutex1 );
+  }
+  return NULL;
+}
+
+void *ATM2()
+{
+  for (int i=0; i<LOOPS; i++) {
+    pthread_mutex_lock( &mutex1 );
+    money = money - 1;
+    pthread_mutex_unlock( &mutex1 );
+  }
+  return NULL;
+}
+
+int main() 
+{
+    int counter=0;
+    while (1)
+    {
+       counter++;
+       pthread_t thread1, thread2;
+
+       pthread_create(&thread1, NULL, ATM1, NULL);
+       pthread_create(&thread2, NULL, ATM2, NULL);
+
+       pthread_join( thread1, NULL);
+       pthread_join( thread2, NULL);
+       if (counter==100){
+        printf("money=%d\n",money);
+        printf("No Errors Heppened");
+        break;
+       }
+    }
+}
---
+++ 0410/race.c
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <pthread.h>
+
+#define LOOPS 10000000
+int money = 100;
+
+void *ATM1()
+{
+  for (int i=0; i<LOOPS; i++) {
+    money = money + 1;
+  }
+  return NULL;
+}
+
+void *ATM2()
+{
+  for (int i=0; i<LOOPS; i++) {
+    money = money - 1;
+  }
+  return NULL;
+}
+
+int main() 
+{
+    int counter=0;
+    while (1)
+    {
+        pthread_t thread1, thread2;
+
+        pthread_create(&thread1, NULL, ATM1, NULL);
+        pthread_create(&thread2, NULL, ATM2, NULL);
+
+        pthread_join( thread1, NULL);
+        pthread_join( thread2, NULL);
+        if(money<0){
+            printf("Error");
+            break;
+        }
+        else
+        {
+            printf("money=%d\n", money);
+        }
+    }
+}
